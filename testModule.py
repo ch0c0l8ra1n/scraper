7eeeabdd7c7b181af1bfb7ff68142fbac6993c95
Rajat Parajuli <073bct532.rajat@pcampus.edu.np>
2019-06-16 16:11:57 +0545

    Renamed files

---
+++ testModule.py
@@ -0,0 +1,24 @@
+import random
+
+class Errors:
+    codeError = 0
+    proxyError = 1
+
+succ = {
+        "success": True,
+        "result" : [random.randint(0,100) for i in range(random.randint(0,100))]
+        }
+
+codeE = {
+        "success": False,
+        "error" : Errors.codeError
+        }
+
+proxyE = {
+        "success": False,
+        "error" : Errors.proxyError
+        }
+
+def work(code,proxy,counters):
+    res = random.choice([ succ] * 1 + [codeE] + [proxyE])
+    return res
